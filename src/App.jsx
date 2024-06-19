5a2828c0635352abc8fa5b69e01ea11e45153c72
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-06-19 17:12:03 +0100

    import the ChooseUser component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
+import ChooseUser from './pages/ChooseUser'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/signup-as' element={<ChooseUser />} />
     </Routes>
   )
 }
