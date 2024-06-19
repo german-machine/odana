a45a2f9035cd58366c6bd99cb65a34254e2ddc0a
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-06-19 16:39:54 +0100

    Imported the SignUp component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
 import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 function App() {
 
   return (
     <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/sign-up' element={<SignUp />} />
     </Routes>
   )
 }
