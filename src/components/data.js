ef9bef243063deebf8379caabf54519b901f6acd
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-06-24 12:24:50 +0100

    added constants for mapping

---
+++ src/components/data.js
@@ -0,0 +1,59 @@
+import odanaLogo from '../assets/images/odanaLogo.png'
+import manWoman from '../assets/images/man-woman.png'
+import twoPhones from '../assets/images/two-phones.png'
+import onTable from '../assets/images/on-table.png'
+import ChooseUser from '../pages/ChooseUser'
+
+export const HOW_IT_WORKS = [
+    {
+        id: '1',
+        label: 'Buyer and seller will create an account',
+        content: "The buyer and seller will register an account with ODANA, after agreeing on a terms and create a transaction in minutes.",
+    },
+    {
+        id: '2',
+        label: 'Buyer deposits funds with ODANA',
+        content: "The buyer deposits the funds in full. ODANA issues a signed, QR-secured letter of comfort to the seller.",
+    },
+    {
+        id: '3',
+        label: 'Seller delivers goods or services',
+        content: "Knowing that the funds are safe with ODANA, the seller is comfortable to deliver the goods or services. Courier or tracking information can also be uploaded.",
+    },
+    {
+        id: '4',
+        label: 'The Buyer inspects and accepts the goods or services',
+        content: "Once the buyer confirms that the agreement has been fulfilled, they authorize ODANA to release payment to the seller.",
+    },
+    {
+        id: '5',
+        label: 'ODANA releases the funds',
+        content: "ODANA releases the funds to the seller on the same day, using Standard Bank’s Host-to-Host technology, closing the transaction.",
+    },
+]
+
+export const GET_STARTED = [
+    {
+        id: '1',
+        img: odanaLogo,
+
+    },
+    {
+        id: '2',
+        img: manWoman,
+        title: 'Shop smart online',
+        content: 'Udana allow various parties to engage in the exchange of goods and services within a sale and purchase agreement teams.'
+    },
+    {
+        id: '3',
+        img: twoPhones,
+        title: 'Secure Your Transaction',
+        content: 'Use our services for all of your online transaction to eliminate fraud'
+    },
+    {
+        id: '4',
+        img: onTable,
+        title: 'Dispute Resolution',
+        content: 'Incase of dissatisfaction of one part, a dispute resolution system is set up to fairly resolve it'
+    },
+]
