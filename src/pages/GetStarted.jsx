7344aa47053f1b811585699d55e2c3653b4ccda8
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-06-24 12:54:48 +0100

    imported Carousel component

---
+++ src/pages/GetStarted.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Carousel from '../components/Carousel'
+
+const GetStarted = () => {
+  return (
+    <div className=" bg-white h-screen relative">
+        <div className="max-w-lg">
+            <Carousel />
+        </div>
+    </div>
+  )
+}
+
+export default GetStarted
