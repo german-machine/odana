c45b3a88ffcb04a3a6ea0a1b3d56557fecbd5a5c
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-06-25 20:03:20 +0100

    added custom animation for the navigation menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+        extend: {
+            animation: {
+                slideIn: 'slideIn 0.5s ease-in-out',
+                slideOut: 'slideOut 0.5s ease-in-out'
+            },
+            keyframes: {
+                slideIn: {
+                    '0%': {
+                        opacity: '0',
+                        transform: 'translateY(-100px)'
+                    },
+                    '100%':{
+                        opacity: '1',
+                        transform: 'translateY(5px)'
+                    }
+                },
+                slideOut: {
+                    '0%': {
+                        opacity: '1',
+                        transform: 'translateY(0px)'
+                    },
+                    '100%': {
+                        opacity: '0',
+                        transform: 'translateY(-100px)'
+                    }
+                }
+            }
+      },
     },
     plugins: [],
   }
