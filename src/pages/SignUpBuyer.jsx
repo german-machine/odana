2d72223cc1c756387f3403f0865b2c89c71e228c
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-06-27 01:54:19 +0100

    styled for medium and desktop devices; function: password must be more than 8 words with lowercase, uppercase, number and special characters; clear all input fields after submiting; trimmed all input fields and all typed letters show as lowercase in the username input field

--- src/pages/SignUpBuyer.jsx
+++ src/pages/SignUpBuyer.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import good from '../assets/images/good.png'
 import eyeoff from '../assets/images/eye-off.png'
-import whiteArrow from '../assets/images/white-arrow.png'
 import google from '../assets/images/google.png'
 import apple from '../assets/images/apple.png'
 import { Link } from 'react-router-dom'
@@ -20,10 +19,10 @@ const SignUp = () => {
         let tempError = {}
 
         if (!inputValue.username) {
-            tempError.username = 'Username is required' 
+            tempError.username = 'Username is required'
         } else if (/\W/.test(inputValue.username)) {
             tempError.username = 'Invalid username'
-        } 
+        }
 
         if (!inputValue.email) {
             tempError.email = 'Email is required'
@@ -33,11 +32,11 @@ const SignUp = () => {
 
         if (!inputValue.password) {
             tempError.password = 'Password is required'
-        } else if (inputValue.password.length < 8) {
-            tempError.password = 'Password too short'
+        } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(inputValue.password)) {
+            tempError.password = 'Password must be more than 8 digits, contain lowercase, uppercase, number and special character'
         }
 
-        
+
         if (!inputValue.number) {
             tempError.number = 'Phone number is required'
         } else if (inputValue.number.length !== 11) {
@@ -56,95 +55,107 @@ const SignUp = () => {
 
     function handleChange(e) {
         const { name, value } = e.target
-        setInputValue({
-            ...inputValue,
-            [name]: value
-        })
+        let updatedValue = { ...inputValue }
+        let trimmedValue = value.trim()
+        if (name === 'username') {
+            updatedValue[name] = value.toLowerCase()
+            e.target.value = trimmedValue.toLowerCase()
+        } else {
+            updatedValue[name] = value
+        }
+        setInputValue(updatedValue)
     }
 
     function handleSubmit(e) {
         e.preventDefault()
         if (validateError()) {
             alert('yeap')
+            setInputValue({
+                username: '',
+                email: '',
+                password: '',
+                confirmPassword: '',
+                number: ''
+            })
         }
     }
 
     return (
-        <div className='w-full bg-white'>
+        <div className='w-full md:h-screen lg:h-full bg-white'>
             <div className="w-[90%] m-auto py-10">
 
                 <div className="w-full mb-5">
-                    <h3 className="text-[26px] font-semibold text-left text-[#030303]">Sign Up (Buyer)</h3>
-                    <p className="text-[16px] text-[#7C7C7C] text-left">Enter your credentials to continue</p>
+                    <h3 className="text-[26px] md:text-[50px] lg:text-[40px] font-semibold text-center text-[#030303]">Sign Up (Buyer)</h3>
+                    <p className="text-[18px] md:text-[30px] lg:text-[22px] text-[#7C7C7C] text-center">Enter your credentials to continue</p>
                 </div>
 
-                <form action='/' onSubmit={handleSubmit} className="w-full">
+                <form action='/' onSubmit={handleSubmit} className="w-full md:w-[80%] m-auto">
 
                     <div className="text-left mb-5 flex flex-col gap-2">
-                        <label htmlFor="username" className="text-[15.04px] text-[#4F4F4F] font-bold text-left">Username</label>
+                        <label htmlFor="username" className="text-[18px] md:text-[24px] text-[#4F4F4F] font-bold text-left">Username</label>
                         <div className="border border-[#BDBDBD] flex items-center justify-between px-2 py-1">
-                            <input onChange={handleChange} value={inputValue.username} type="text" name='username' placeholder='Joseph Johnson' required className="py-2 w-full bg-transparent focus:outline-none text-[#4F4F4F]"/>
-                            <img src={good} alt="" className="hidden"/>
+                            <input onChange={handleChange} value={inputValue.username} type="text" name='username' placeholder='Joseph Johnson' required className="py-2 w-full bg-transparent focus:outline-none text-[#4F4F4F] md:text-[24px]" />
+                            <img src={good} alt="" className="hidden" />
                         </div>
                         {error.username && <p className='text-red-600'>{error.username}</p>}
                     </div>
 
                     <div className="text-left mb-5 flex flex-col gap-2">
-                        <label htmlFor="email" className="text-[15.04px] text-[#4F4F4F] font-bold text-left">Email Address</label>
+                        <label htmlFor="email" className="text-[18px] md:text-[24px] text-[#4F4F4F] font-bold text-left">Email Address</label>
                         <div className="border border-[#BDBDBD] flex items-center justify-between px-2 py-1">
-                            <input onChange={handleChange} value={inputValue.email} type="email" name='email' placeholder='odana@gmail.com' required className="py-2 w-full bg-transparent focus:outline-none text-[#4F4F4F]"/>
-                            <img src={good} alt="" className="hidden"/>
+                            <input onChange={handleChange} value={inputValue.email} type="email" name='email' placeholder='odana@gmail.com' required className="py-2 w-full bg-transparent focus:outline-none text-[#4F4F4F] md:text-[24px]" />
+                            <img src={good} alt="" className="hidden" />
                         </div>
                         {error.email && <p className='text-red-600'>{error.email}</p>}
                     </div>
 
                     <div className="text-left mb-5 flex flex-col gap-2">
-                        <label htmlFor="number" className="text-[15.04px] text-[#4F4F4F] font-bold text-left">Phone Number</label>
+                        <label htmlFor="number" className="text-[18px] md:text-[24px] text-[#4F4F4F] font-bold text-left">Phone Number</label>
                         <div className="border border-[#BDBDBD] flex items-center justify-between px-2 py-1">
-                            <input onChange={handleChange} value={inputValue.number} type="tel" name='number' placeholder='08123456789' required className="py-2 w-full bg-transparent focus:outline-none text-[#4F4F4F]"/>
-                            <img src={good} alt="" className="hidden"/>
+                            <input onChange={handleChange} value={inputValue.number} type="tel" name='number' placeholder='08123456789' required className="py-2 w-full bg-transparent focus:outline-none text-[#4F4F4F] md:text-[24px]" />
+                            <img src={good} alt="" className="hidden" />
                         </div>
                         {error.number && <p className='text-red-600'>{error.number}</p>}
                     </div>
 
                     <div className="text-left mb-5 flex flex-col gap-2">
-                        <label htmlFor="password" className="text-[15.04px] text-[#4F4F4F] font-bold text-left">Password</label>
+                        <label htmlFor="password" className="text-[18px] md:text-[24px] text-[#4F4F4F] font-bold text-left">Password</label>
                         <div className="border border-[#BDBDBD] flex items-center justify-between px-2 py-1">
-                            <input onChange={handleChange} value={inputValue.password} type="passwor" name='password' placeholder='**********' required className="py-2 w-full bg-transparent focus:outline-none text-[#4F4F4F]"/>
-                            <img src={eyeoff} alt="" className="hidden"/>
+                            <input onChange={handleChange} value={inputValue.password} type="passwor" name='password' placeholder='**********' required className="py-2 w-full bg-transparent focus:outline-none text-[#4F4F4F] md:text-[24px]" />
+                            <img src={eyeoff} alt="" className="hidden" />
                         </div>
                         {error.password && <p className='text-red-600'>{error.password}</p>}
                     </div>
 
                     <div className="text-left mb-5 flex flex-col gap-2">
-                        <label htmlFor="confirmPassword" className="text-[15.04px] text-[#4F4F4F] font-bold text-left">Confirm Password</label>
+                        <label htmlFor="confirmPassword" className="text-[18px] md:text-[24px] text-[#4F4F4F] font-bold text-left">Confirm Password</label>
                         <div className="border border-[#BDBDBD] flex items-center justify-between px-2 py-1">
-                            <input onChange={handleChange} value={inputValue.confirmPassword} type="passwor" name='confirmPassword' placeholder='**********' required className="py-2 w-full bg-transparent focus:outline-none text-[#4F4F4F]"/>
-                            <img src={eyeoff} alt="" className="hidden"/>
+                            <input onChange={handleChange} value={inputValue.confirmPassword} type="passwor" name='confirmPassword' placeholder='**********' required className="py-2 w-full bg-transparent focus:outline-none text-[#4F4F4F] md:text-[24px]" />
+                            <img src={eyeoff} alt="" className="hidden" />
                         </div>
                         {error.confirmPassword && <p className='text-red-600'>{error.confirmPassword}</p>}
                     </div>
 
-                    <p className="text-[15px] mb-3 font-normal text-[#7C7C7C] text-center">By continuing, you agree to our <span className="text-[#53B175]">Terms of Service</span> and <span className="text-[#53B175]">Privacy Policy</span></p>
+                    <p className="text-[16px] md:text-[18px] mb-3 font-normal text-[#7C7C7C] text-center">By continuing, you agree to our <span className="text-[#53B175]">Terms of Service</span> and <span className="text-[#53B175]">Privacy Policy</span></p>
 
-                    <button type='submit' className="w-full border bg-[#299B56] text-center flex justify-center rounded-3xl items-center mb-5">
+                    <button type='submit' className="w-full md:w-[30%] md:text-[19px] m-auto py-2 md:py-3 border bg-[#299B56] text-center flex justify-center rounded-3xl items-center mb-5">
                         Submit
                     </button>
 
-                    <div className='w-full flex items-center justify-center text-center'>
+                    <div className='w-full md:w-[70%] m-auto flex items-center justify-center text-center gap-1'>
                         <div className='w-[100%] h-[1px] bg-black'></div>
-                        <p className='w-[20%] text-[14px] text-[#030303]'>or</p>
+                        <p className='w-[15%] text-[14px] text-[#030303]'>or</p>
                         <div className='w-[100%] h-[1px] bg-black'></div>
                     </div>
 
-                    <Link className="border rounded-3xl border-[#53B175] flex items-center justify-center gap-2 mt-5 mb-5">
+                    <Link className="w-full md:w-[70%] m-auto border rounded-3xl border-[#53B175] flex items-center justify-center gap-2 mt-5 mb-5">
                         <img src={google} alt="" />
-                        <Link className="text-[14px] font-semibold text-[#299B56] py-2">Sign Up with Google</Link>
+                        <Link className="text-[16px] md:text-[19px] font-semibold text-[#299B56] py-2 md:py-3">Sign Up with Google</Link>
                     </Link>
 
-                    <Link className="border rounded-3xl border-[#53B175] flex items-center justify-center gap-2">
+                    <Link className="w-full md:w-[70%] m-auto border rounded-3xl border-[#53B175] flex items-center justify-center gap-2">
                         <img src={apple} alt="" />
-                        <p className="text-[14px] font-semibold text-[#299B56] py-2">Sign Up with Apple</p>
+                        <p className="text-[16px] md:text-[19px] font-semibold text-[#299B56] py-2 md:py-3">Sign Up with Apple</p>
                     </Link>
                 </form>
             </div>
